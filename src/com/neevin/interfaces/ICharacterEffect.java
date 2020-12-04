5581143b6f89e34355ba804d1aa197832949b762
Kirill Neevin <50864661+neevink@users.noreply.github.com>
2020-12-04 18:53:54 +0600

    Update ICharacterEffect.java

--- src/com/neevin/interfaces/ICharacterEffect.java
+++ src/com/neevin/interfaces/ICharacterEffect.java
@@ -2,6 +2,7 @@ package com.neevin.interfaces;
 
 import com.neevin.characters.BookCharacter;
 
+// Используется, как функциональный интерфейс
 public interface ICharacterEffect {
     void invoke(BookCharacter bookCharacter);
 }
